4370e62904316dc784a8b8939d83e84e81dd4227
Asif Ahmed Khan <93794678+asif7480@users.noreply.github.com>
2024-08-22 12:35:15 +0000

    added date methods

---
+++ builtinDateMethods.js
@@ -0,0 +1,19 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let createdDate = new Date(2021, 2, 20)
+console.log(createdDate);
+console.log(createdDate.toLocaleString());
+
+let timeStamp = Date.now()
+console.log(timeStamp);
+console.log(createdDate.getTime());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+
+
